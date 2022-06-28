65feea013b16c7510fbfde20856a002896e33ad8
seungmin1592 <tmdals1592@naver.com>
2022-06-28 23:04:09 +0900

    vue project

---
+++ vue3/project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Hello {{ userName }}</h1>
+        <p>{{message}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            userName: 'John Doe',
+            messgae: 'Welcome 개발자의 품격',
+            arr: [],
+            obj: {}
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
